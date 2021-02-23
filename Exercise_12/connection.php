1e64f32b431040fa375f9a1b18f910a523c52920
Kripa Murugan <74702118+kripamurugan@users.noreply.github.com>
2021-02-23 15:56:49 +0530

    Update connection.php

--- Exercise_12/connection.php
+++ Exercise_12/connection.php
@@ -1,1 +1,16 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="first";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if (!$conn)
+{
+	echo "Connection Failed";
+}
+else
+{
+	
+}
+?>
 
